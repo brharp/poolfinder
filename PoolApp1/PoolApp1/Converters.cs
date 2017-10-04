4895195896940a896d7e4db929c9fdb39b96444e
Brent Harp <brharp@uoguelph.ca>
2017-10-04 19:38:13 -0400

    Commit Windows app

---
+++ PoolApp1/PoolApp1/Converters.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace PoolApp1
+{
+    public class TimeFormat : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            DateTime dt = new DateTime() + (TimeSpan)value;
+            return dt.ToString("h:mm tt");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class DurationFormat : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var timeSpanValue = (TimeSpan)value;
+            var totalHours = timeSpanValue.TotalHours;
+            var pluralize = (totalHours != 1);
+            return String.Format("{0} hour{1}", totalHours, pluralize ? "s" : "");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
---
+++ PoolApp1/PoolApp1/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PoolApp1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<Schedule> _items = new ObservableCollection<Schedule>();
+        private DateTime today;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.today = DateTime.Now;
+        }
+
+        public IEnumerable<Schedule> Items
+        {
+            get
+            {
+                return Schedule.Schedules
+                    .Where(i => i.DayOfWeek == DateTime.Now.DayOfWeek)
+                    .Where(i => DateTime.Today + i.StartTime >= DateTime.Now)
+                    .OrderBy(i => i.StartTime);
+            }
+        }
+
+        public IEnumerable<Schedule> Tomorrow
+        {
+            get
+            {
+                return Schedule.Schedules
+                    .Where(i => i.DayOfWeek == DateTime.Now.AddDays(1).DayOfWeek)
+                    .OrderBy(i => i.StartTime);
+            }
+        }
+    }
+}
